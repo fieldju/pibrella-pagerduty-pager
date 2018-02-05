1691261be7d5bed6855a313e3ef484c761304436
Justin Field <fieldju@gmail.com>
2018-02-04 22:18:46 -0800

    Use construction injection

--- src/main/java/com/fieldju/pager/controller/PagerWebHook.java
+++ src/main/java/com/fieldju/pager/controller/PagerWebHook.java
@@ -3,23 +3,21 @@ package com.fieldju.pager.controller;
 import com.fieldju.pager.model.pagerduty.webhook.Trigger;
 import com.fieldju.pager.service.PagerDuty;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class PagerWebHook {
 
-    @Autowired
-    PagerDuty pagerDuty;
+    private final PagerDuty pagerDuty;
 
-    @RequestMapping(
-            method = RequestMethod.POST,
-            path = "/pager-duty-hook"
-    )
+    @Autowired
+    public PagerWebHook(PagerDuty pagerDuty) {
+        this.pagerDuty = pagerDuty;
+    }
+
+    @PostMapping("/pagerduty-hook" )
     public void processPagerDutyWebHook(@RequestBody Trigger trigger, HttpServletResponse response) {
         pagerDuty.processTriggerAsync(trigger);
         response.setStatus(HttpServletResponse.SC_NO_CONTENT);
--- src/main/java/com/fieldju/pager/service/PagerDuty.java
+++ src/main/java/com/fieldju/pager/service/PagerDuty.java
@@ -2,6 +2,8 @@ package com.fieldju.pager.service;
 
 import com.fieldju.pager.model.pagerduty.webhook.Message;
 import com.fieldju.pager.model.pagerduty.webhook.Trigger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ExecutorService;
@@ -10,6 +12,8 @@ import java.util.concurrent.Executors;
 @Component
 public class PagerDuty {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     private final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     public void processTriggerAsync(Trigger trigger) {
@@ -17,6 +21,6 @@ public class PagerDuty {
     }
 
     private void processMessage(Message message) {
-
+        log.info("Received message: {}", message.getType());
     }
 }
